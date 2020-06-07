e4f82509446809969e7900c7367cb2d0da45daad
Jake Sasser <sasserjacob@gmail.com>
2020-06-06 22:48:25 -0600

    update controllers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,50 @@
 class TripsController < ApplicationController
+
   def index
+    @trips = Trip.all        
   end
 
   def show
+    # @trip = Trip.find(params[:id])
   end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def edit
+    # @trip = Trip.find(params[:id])
   end
+
+  def update    
+    @trip = Trip.find(params[:id])
+    if @trip.update(trip_params)
+      redirect_to trips_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Trip.find(params[:id]).destroy
+    redirect_to trips_path
+  end
+
+  private
+   def trip_params
+    params.require(:trip).permit(:name, :start_date, :end_date)
+   end
+
+   def set_trip
+    @trip = Trip.find(params[:id])
+   end
 end
